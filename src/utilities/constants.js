07960d2ad1e2e608e252907d58f4d30354e7f515
vijay <mohitevijaysinh8379@gmail.com>
2023-10-15 09:41:35 +0530

    Change

---
+++ src/utilities/constants.js
@@ -0,0 +1,7 @@
+export const Product_API =
+  "https://academics.newtonschool.co/api/v1/ecommerce/clothes/products";
+export const projectId = "6zhljr4m9pjk";
+export const ProductInfo_API =
+  "https://academics.newtonschool.co/api/v1/ecommerce/product/";
+export const EachOrder_API =
+  "https://academics.newtonschool.co/api/v1/ecommerce/order/";
